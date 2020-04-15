b38e1f5348c3102775443f6ac267dcf13588086b
Piotr Socha <piotrsocha01@gmail.com>
2020-04-15 17:03:12 +0200

    Fixed and added docstring to Block

--- block.py
+++ block.py
@@ -4,20 +4,28 @@ import json
 
 class Block:
     """
-        Block class constructor.
+        Block class.
+    """
+
+    def __init__(self, id: int, transactions: list, datetime: str, prev_hash: str):
+        """
+        Block class constructor
         :param id: ID of the block, unique
         :param transactions: List of transactions
         :param datetime: Date and time of block generation
         :param prev_hash: Hash of previous block
-    """
-    def __init__(self, id: int, transactions: list, datetime: str, prev_hash: str):
+        """
 
         self.id = id
         self.transactions = transactions
         self.datetime = datetime
         self.prev_hash = prev_hash
-    
+
     def compute_hash(self) -> str:
+        """
+        Computes sha256 hash of the block.
+        """
+
         block = json.dumps(self.__dict__, sort_keys=True)
         sha = hashlib.sha256(block.encode()).hexdigest()
         return sha
