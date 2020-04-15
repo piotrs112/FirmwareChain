a0f82b117497ec334fad4869bca595777ffc5c27
Piotr Socha <piotrsocha01@gmail.com>
2020-04-15 21:09:29 +0200

    Added mine function and fixed adding transactions

--- blockchain.py
+++ blockchain.py
@@ -4,6 +4,8 @@ from block import Block
 
 
 class Blockchain:
+    DIFFICULTY = 2
+
     def __init__(self):
         """
         Blockchain class constructor
@@ -11,7 +13,7 @@ class Blockchain:
 
         self.chain = []
         self.pending_transactions = []
-        genesis_block = Block(0,[],"0","0")
+        genesis_block = Block(0, [], "0", "0")
         self.chain.append(genesis_block)
 
     def add_transaction(self, transaction: dict):
@@ -20,8 +22,9 @@ class Blockchain:
         :param transaction: Dictionary with keys: version, filename, file_hash
         """
 
-        self.pending_transactions.append(dict)
+        self.pending_transactions.append(transaction)
 
+    @property
     def last_block(self) -> Block:
         """
         Returns the last block of the chain
@@ -29,4 +32,18 @@ class Blockchain:
 
         return self.chain[-1]
 
-    
+    def mine(self):
+        """
+        Mines a new block with a Proof of Work and adds it to the chain.
+        """
+
+        new_id = self.last_block.block_id + 1
+        prev_hash = self.last_block.compute_hash()
+        block = Block(new_id, self.pending_transactions,
+                      str(time.time()), prev_hash)
+        computed_hash = block.compute_hash()
+        while not computed_hash.startswith('0' * self.DIFFICULTY):
+            block.nonce = block.nonce + 1
+            computed_hash = block.compute_hash()
+
+        self.chain.append(block)
