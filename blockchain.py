efa511d0f2616b535aeb5fb80309483fe8a26063
Piotr Socha <piotrsocha01@gmail.com>
2020-04-16 14:51:36 +0200

    Modified mine function, added chain verification

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,5 @@
 import time
+from typing import Tuple
 
 from block import Block
 
@@ -24,6 +25,16 @@ class Blockchain:
 
         self.pending_transactions.append(transaction)
 
+    def proof_of_work(self, block) -> Tuple[Block, str]:
+        """
+        Computes hash until it has a proper number of leading zeros by increasing nonce.
+        """
+        computed = block.compute_hash()
+        while not computed.startswith('0' * self.DIFFICULTY):
+            block.nonce = block.nonce + 1
+            computed = block.compute_hash()
+        return block, computed
+
     @property
     def last_block(self) -> Block:
         """
@@ -37,13 +48,17 @@ class Blockchain:
         Mines a new block with a Proof of Work and adds it to the chain.
         """
 
-        new_id = self.last_block.block_id + 1
-        prev_hash = self.last_block.compute_hash()
-        block = Block(new_id, self.pending_transactions,
-                      str(time.time()), prev_hash)
-        computed_hash = block.compute_hash()
-        while not computed_hash.startswith('0' * self.DIFFICULTY):
-            block.nonce = block.nonce + 1
-            computed_hash = block.compute_hash()
+        if self.pending_transactions != []:
+            new_id = self.last_block.block_id + 1
+            prev_hash = self.last_block.compute_hash()
+            block = Block(new_id, self.pending_transactions,
+                          str(time.time()), prev_hash)
+            block, new_hash = self.proof_of_work(block)
+            self.chain.append(block)
+            self.pending_transactions = []
 
-        self.chain.append(block)
+    def verify_chain(self) -> bool:
+        for i in range(1, len(self.chain)):
+            if self.chain[i].prev_hash != self.chain[i-1].compute_hash():
+                return False
+        return True
