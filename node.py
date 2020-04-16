e5ff9365e0409abcb9958ac981c9dc5ca959e465
Piotr Socha <piotrsocha01@gmail.com>
2020-04-16 15:34:57 +0200

    Initial web interface

---
+++ node.py
@@ -0,0 +1,24 @@
+import json
+import sys
+
+from flask import Flask
+
+from blockchain import Blockchain
+
+app = Flask(__name__)
+b = Blockchain()
+
+@app.route('/', methods=['GET'])
+def index() -> str:
+    html = ""
+    for block in b.chain:
+        html += str(block)
+        html += "\n"
+    return html
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+       port = 5000
+    else:
+       port = sys.argv[-1]
+    app.run(port=port)
