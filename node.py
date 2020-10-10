6e109ef85bc392ac96635391c3b8acc7c2bc1df4
piotrs112 <piotrsocha01@gmail.com>
2020-10-10 13:42:03 +0200

    Added console-only node script

--- node.py
+++ node.py
@@ -18,13 +18,12 @@ class Node:
 
         # Attach listener to connection event
         self.sock.on('connect', self.on_connect)
+        self.sock.once('connect', self.once_connect)
 
     def handle_incoming(self, msg: py2p.base.Message, handler):
         """
         Handles incoming messages
         """
-        print("I got sth")
-        print(msg.packets)
         if msg.packets[0] == 'new_transaction':
             for t in msg.packets[1:]:
                 try:
@@ -33,6 +32,57 @@ class Node:
                     print(e.with_traceback())
         elif msg.packets[0] == 'mining_finished':
             print("Stop mining! Someone else was first :(")
+            # Handle mined notion/block
 
-    def on_connect(self, socket: py2p.MeshSocket):
-        print(f"New connection, total: {len(socket.routing_table)}")
+    def on_connect(self, sock: py2p.MeshSocket):
+        print(f"New connection, total: {len(self.sock.routing_table)}")
+
+    def once_connect(self, sock):
+        #todo get longest blockchain
+        pass
+    
+    def discovery(self):
+        #todo ping same port on every ip on lan
+        pass
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+if __name__ == "__main__":
+    # Run without arguments
+    if len(sys.argv) == 1:
+        port = 6000
+    # Run with arguments, last is the port nr
+    else:
+        port = int(sys.argv[-1])
+
+    node = Node(port=port)
+    print(bcolors.OKGREEN + "Node setup done." + bcolors.ENDC)
+
+    if port != 6000:
+        try:
+            node.sock.connect('0.0.0.0', 6000) # todo change when discovery is implmented
+        except ConnectionRefusedError:
+            print(bcolors.FAIL+"Couldn't connect to peer"+bcolors.ENDC)
+
+    while i:=input():
+        if i == 'exit' or i == 'e':
+            node.sock.close()
+            exit()
+        elif i == 'status':
+            print(bcolors.OKBLUE+ node.sock.status+bcolors.ENDC)
+        elif i == 'peers':
+            print(bcolors.OKBLUE+node.sock.routing_table+bcolors.ENDC)
+        elif i.startswith('connect '):
+            ip, port = i.split(" ")[1].split(':')
+            try:
+                node.sock.connect(str(ip), int(port))
+            except ConnectionRefusedError:
+                print(bcolors.FAIL+"Couldn't connect to peer"+bcolors.ENDC)
